63bc23301965ef4fe74b68d25947c9a3ed062e7c
Aman Kazmi <amankazmi257@gmail.com>
2025-07-16 16:40:22 +0530

    Implement adding sweets to pass test

--- sweetShop.js
+++ sweetShop.js
@@ -4,7 +4,14 @@ class SweetShop {
     this.nextId = 1001;
   }
 
-  
+  addSweet(sweet) {
+    sweet.id = this.nextId++;
+    this.sweets.push(sweet);
+  }
+
+  getSweets() {
+    return this.sweets;
+  }
 }
 
 module.exports = SweetShop; 
