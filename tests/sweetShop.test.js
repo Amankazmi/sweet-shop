7919435442c5c34977765c15637c09ddac83d410
Aman Kazmi <amankazmi257@gmail.com>
2025-07-16 16:44:07 +0530

    Add failing test for deleting sweets

--- tests/sweetShop.test.js
+++ tests/sweetShop.test.js
@@ -8,4 +8,11 @@ describe('SweetShop', () => {
     expect(shop.getSweets().length).toBe(1);
   });
 
+   test('should delete a sweet', () => {
+    const shop = new SweetShop();
+    shop.addSweet({ name: 'Lollipop', price: 1.5, type: 'candy', quantity: 10 });
+    shop.deleteSweet('Lollipop');
+    expect(shop.getSweets().length).toBe(0);
+  });
+
 }); 
